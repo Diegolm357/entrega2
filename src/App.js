fe0bc0f6976dbe7049a6351cac2554eac4f6c837
diegol <diegolm73@yahoo.com.ar>
2021-06-09 20:32:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbars from './components/Navbars';
+
+function App() {
+  return (
+    <div>
+     <header>
+     <Navbars/>
+     </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbars.js
@@ -0,0 +1,33 @@
+import { Button ,Image, Nav, Navbar, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import vino from '../vino.jpg';
+function Navbars() {
+  return (
+    <>  
+   
+    <Navbar bg="light" expand="lg">
+    <Image classname="logo" src={vino} style={{width:35}} />
+      <Navbar.Brand href="#home">INICIO</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">BLANCOS</Nav.Link>
+      <Nav.Link href="#home">TINTOS</Nav.Link>
+      <Nav.Link href="#home">ESPUMANTES</Nav.Link>
+      <NavDropdown title="Contacto" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Nosotros</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Vendedores</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Ubicacion</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">About</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Ingrese su busqueda" className="mr-sm-2" />
+      <Button variant="outline-success">Buscar</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+</>
+  );
+}
+export default Navbars;
